b5b67b1867fc117972fa837bab31e86c423aece7
Ashish Kumar <ashishkumat818@gmail.com>
2024-10-30 16:36:26 +0530

    backend

---
+++ ems-backend/src/main/java/com/ems/ems_backend/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.ems.ems_backend.service;
+
+import java.util.List;
+import com.ems.ems_backend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
