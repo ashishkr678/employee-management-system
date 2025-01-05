2d4010c0d4f4a396d668c9292be0c6be84b88211
Ashish Kumar <ashishkumat818@gmail.com>
2025-01-05 21:04:41 +0530

    updated

--- ems-backend/src/test/java/com/ems/ems_backend/EmsBackendApplicationTests.java
+++ ems-backend/src/test/java/com/ems/ems_backend/EmsBackendApplicationTests.java
@@ -1,10 +1,18 @@
 package com.ems.ems_backend;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.ems.ems_backend.security.DotenvConfig;
+
 @SpringBootTest
 class EmsBackendApplicationTests {
+
+	@BeforeAll
+    static void setup() {
+        DotenvConfig.loadSystemProperties();
+    }
 
 	@Test
 	void contextLoads() {
