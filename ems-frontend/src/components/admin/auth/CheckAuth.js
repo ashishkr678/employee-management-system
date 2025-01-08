a54a40ddb9c8da2cf1fe80559be4f9a81ec39957
Ashish Kumar <ashishkumat818@gmail.com>
2025-01-08 20:15:08 +0530

    ok

--- ems-frontend/src/components/admin/auth/CheckAuth.js
+++ ems-frontend/src/components/admin/auth/CheckAuth.js
@@ -3,7 +3,11 @@ import api from "../../../apiConfig/ApiConfig";
 export const CheckAuth = async () => {
   try {
     const response = await api.get("/admin/check-auth");
-    return response.status === 200;
+    
+    if (response.status === 200 && response.data.message === "Authenticated") {
+      return true;
+    }
+    return false;
   } catch (error) {
     return false;
   }
